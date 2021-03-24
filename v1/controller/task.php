a13adfcd90c270006522832e5b5979c365a455af
Olle Nilsson <olle.nilsson@medieinstitutet.se>
2021-03-24 19:11:58 +0100

    get complete or incomplete tasks done

--- v1/controller/task.php
+++ v1/controller/task.php
@@ -84,7 +84,38 @@ if (array_key_exists("taskid", $_GET)) {
         }
 
     } elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+        try {
+            $query = $writeDB->prepare('DELETE FROM tbltasks WHERE id = :taskid');
+            $query->bindParam(":taskid", $taskid, PDO::PARAM_INT);
+            $query->execute();
 
+            $rowCount = $query->rowCount();
+
+            //Om den inte deletas
+            if ($rowCount === 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(404);
+                $response->setSuccess(false);
+                $response->addMessage("Task not found");
+                $response->send();
+                exit;
+            }
+            //Deleted
+            $response = new Response();
+            $response->setHttpStatusCode(200);
+            $response->setSuccess(true);
+            $response->addMessage("Task deleted");
+            $response->send();
+            exit;
+        } catch (PDOEXception $ex) {
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Failed to delete task");
+            $response->send();
+            exit;
+
+        }
     } elseif ($_SERVER['REQUEST_METHOD'] === 'PATCH') {
 
     } else {
@@ -96,4 +127,70 @@ if (array_key_exists("taskid", $_GET)) {
         exit;
     }
 
+} elseif (array_key_exists("completed", $_GET)) {
+
+    $completed = $_GET['completed'];
+    if ($completed !== 'Y' && $completed !== 'N') {
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        $response->addMessage("Completed filter must be Y or N");
+        $response->send();
+        exit;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        try {
+            $query = $readDB->prepare('SELECT id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") as deadline, completed FROM tbltasks WHERE completed = :completed');
+            $query->bindParam(":completed", $completed, PDO::PARAM_STR);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            $taskArray = array();
+
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $task = new Task($row['id'], $row['title'], $row['description'], $row['deadline'], $row['completed']);
+
+                $taskArray[] = $task->returnTaskAsArray();
+            }
+
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['tasks'] = $taskArray;
+
+            $response = new Response();
+            $response->setHttpStatusCode(200);
+            $response->setSuccess(true);
+            $response->toCache(true);
+            $response->setData($returnData);
+            $response->send();
+            exit;
+
+        } catch (TaskException $ex) {
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage($ex->getMessage());
+            $response->send();
+            exit;
+        } catch (PDOEXception $ex) {
+            error_log("Database query error - " . $ex, 0); // 0 - error log for PHP
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Failed to get tasks");
+            $response->send();
+            exit;
+
+        }
+
+    } else {
+        $response = new Response();
+        $response->setHttpStatusCode(405);
+        $response->setSuccess(false);
+        $response->addMessage("Request method not allowed");
+        $response->send();
+        exit;
+    }
 }
