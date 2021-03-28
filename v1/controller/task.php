cff1fb253f22a9f280657d7dcea1b8cdffd3bb0f
Olle Nilsson <olle.nilsson@medieinstitutet.se>
2021-03-28 16:14:51 +0200

    create task working

--- v1/controller/task.php
+++ v1/controller/task.php
@@ -193,4 +193,280 @@ if (array_key_exists("taskid", $_GET)) {
         $response->send();
         exit;
     }
+} elseif (array_key_exists("page", $_GET)) {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $page = $_GET['page'];
+
+        if ($page == '' || !is_numeric($page)) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage("Page number cannot be blank and must be numeric");
+            $response->send();
+            exit;
+        }
+
+        $limitPerPage = 20;
+
+        try {
+
+            $query = $readDB->prepare('SELECT COUNT(id) as totalNoOfTasks from tbltasks');
+            $query->execute();
+
+            $row = $query->fetch(PDO::FETCH_ASSOC);
+
+            $tasksCount = intval($row['totalNoOfTasks']);
+
+            $numOfPages = ceil($tasksCount / $limitPerPage); //ceil gör att numret alltid rundas uppåt, 1.5 blir 2 tex.
+
+            if ($numOfPages == 0) {
+                $numOfPages = 1;
+            }
+
+            if ($page > $numOfPages || $page == 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(404);
+                $response->setSuccess(false);
+                $response->addMessage("Page not found");
+                $response->send();
+                exit;
+            }
+
+            $offset = ($page == 1 ? 0 : ($limitPerPage * ($page - 1))); // ? = THEN ,   :    = ELSE
+
+            $query = $readDB->prepare('SELECT id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") as deadline, completed from tbltasks limit :pglimit offset :offset');
+            $query->bindParam(':pglimit', $limitPerPage, PDO::PARAM_INT);
+            $query->bindParam(':offset', $offset, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            $taskArray = array();
+
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $task = new Task($row['id'], $row['title'], $row['description'], $row['deadline'], $row['completed']);
+
+                $taskArray[] = $task->returnTaskAsArray();
+
+            }
+
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['total_rows'] = $tasksCount;
+            $returnData['total_pages'] = $numOfPages;
+            ($page < $numOfPages ? $returnData['has_next_page'] = true : $returnData['has_next_page'] = false);
+            ($page > 1 ? $returnData['has_previous_page'] = true : $returnData['has_next_page'] = false);
+            $returnData['tasks'] = $taskArray;
+
+            $response = new Response();
+            $response->setHttpStatusCode(200);
+            $response->setSuccess(true);
+            $response->toCache(true);
+            $response->setData($returnData);
+            $response->send();
+            exit;
+        } catch (TaskException $ex) {
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage($ex->getMessage());
+            $response->send();
+            exit;
+        } catch (PDOEXception $ex) {
+            error_log("Database query error - " . $ex, 0);
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Failed to get tasks");
+            $response->send();
+            exit;
+        }
+
+    } else {
+        $response = new Response();
+        $response->setHttpStatusCode(405);
+        $response->setSuccess(false);
+        $response->addMessage("Request method not allowed");
+        $response->send();
+        exit;
+    }
+} elseif (empty($_GET)) {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+        try {
+            $query = $readDB->prepare('SELECT id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") as deadline, completed FROM tbltasks');
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            $taskArray = array();
+
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $task = new Task($row['id'], $row['title'], $row['description'], $row['deadline'], $row['completed']);
+                $taskArray[] = $task->returnTaskAsArray();
+            }
+
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['tasks'] = $taskArray;
+
+            $response = new Response();
+            $response->setHttpStatusCode(200);
+            $response->setSuccess(true);
+            $response->toCache(true);
+            $response->setData($returnData);
+            $response->send();
+            exit;
+
+        } catch (TaskException $ex) {
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage($ex->getMessage());
+            $response->send();
+            exit;
+        } catch (PDOEXception $ex) {
+            error_log("Database query error - " . $ex, 0);
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Failed to get tasks");
+            $response->send();
+            exit;
+        }
+
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        try {
+
+            if ($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                $response->addMessage("Content type header is not set to JSON");
+                $response->send();
+                exit;
+            }
+
+            $rawPOSTData = file_get_contents('php://input');
+
+            //Om det inte är json data Error.
+            if (!$jsonData = json_decode($rawPOSTData)) {
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                $response->addMessage("Request body is not valid JSON");
+                $response->send();
+                exit;
+            }
+
+            if (!isset($jsonData->title) || !isset($jsonData->completed)) {
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                (!isset($jsonData->title) ? $response->addMessage("Title field is mandatory and must be provided") : false); // if statement för error meddelanden
+                (!isset($jsonData->completed) ? $response->addMessage("Completed field is mandatory and must be provided") : false);
+                $response->send();
+                exit;
+            }
+
+            //Null första värdet, id sätts av databasen., if statement för desc och deadline för dom är inte obligatoriska
+            $newTask = new Task(null, $jsonData->title, (isset($jsonData->description) ? $jsonData->description : null), (isset($jsonData->deadline) ? $jsonData->deadline : null), $jsonData->completed);
+
+            $title = $newTask->getTitle();
+            $description = $newTask->getDescription();
+            $deadline = $newTask->getDeadline();
+            $completed = $newTask->getCompleted();
+
+            //Formatera deadline till rätt datum-format
+            $query = $writeDB->prepare('INSERT INTO tbltasks (title, description, deadline, completed) VALUES(:title, :description, STR_TO_DATE(:deadline, \'%d/%m/%Y %H:%i\'), :completed)');
+            $query->bindParam(":title", $title, PDO::PARAM_STR);
+            $query->bindParam(":description", $description, PDO::PARAM_STR);
+            $query->bindParam(":deadline", $deadline, PDO::PARAM_STR);
+            $query->bindParam(":completed", $completed, PDO::PARAM_STR);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            if ($rowCount === 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Failed to create task");
+                $response->send();
+                exit;
+            }
+
+            $lastTaskID = $writeDB->LastInsertId();
+
+            $query = $writeDB->prepare('SELECT id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") as deadline, completed FROM tbltasks where id = :taskid');
+            $query->bindParam(':taskid', $lastTaskID, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            if ($rowCount === 0) {
+                $response = new response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Failed to retrive task after creation");
+                $response->send();
+                exit;
+            }
+
+            $taskArray = array();
+
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $task = new Task($row['id'], $row['title'], $row['description'], $row['deadline'], $row['completed']);
+                $taskArray[] = $task->returnTaskAsArray();
+            }
+
+            //Varje gång man skapar något ska man returnera det till klienten i en API.
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['tasks'] = $taskArray;
+
+            $response = new Response();
+            $response->setHttpStatusCode(201); // Successful create.
+            $response->setSuccess(true);
+            $response->addMessage("Task created");
+            $response->setData($returnData);
+            $response->send();
+            exit;
+        } catch (TaskException $ex) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage($ex->getMessage());
+            $response->send();
+            exit;
+
+        } catch (PDOEXception $ex) {
+            error_log("Database query error" . $ex, 0);
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Failed to insert task into database - check submitted data for errors");
+            $response->send();
+            exit;
+
+        }
+
+    } else {
+        $response = new Response();
+        $response->setHttpStatusCode(405);
+        $response->setSuccess(false);
+        $response->addMessage("Request method not allowed");
+        $response->send();
+        exit;
+
+    }
+
+} else {
+    $response = new Response();
+    $response->setHttpStatusCode(404);
+    $response->setSuccess(false);
+    $response->addMessage("Endpoint not found");
+    $response->send();
+    exit;
 }
